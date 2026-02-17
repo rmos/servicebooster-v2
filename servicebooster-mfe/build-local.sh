93fa6be6b3da0c38f4217d72e4fe835636aeb9a7
Ruben Mosquera <ruben.mosquera@inetum.com>
2026-02-17 11:10:17 +0100

    fix install

--- servicebooster-mfe/build-local.sh
+++ servicebooster-mfe/build-local.sh
@@ -3,6 +3,6 @@ docker build -f docker/ireland.Dockerfile -t mf-ireland:prod .
 docker build -f docker/portugal.Dockerfile -t mf-portugal:prod .
 #docker compose -f docker/docker-compose.yml build --no-cache
 docker compose -p service-booster-v2 -f docker/docker-compose.yml up -d
-#docker compose -f docker/docker-compose.yml down -v
+#docker compose -p service-booster-v2 -f docker/docker-compose.yml down -v
 #docker run -d --name sb-shell -dit --restart always -p 8080:80 sb-shell:prod
 #http://localhost:8080/v2/?mf=all&mfEnv=prod
